e1b8131b6bbce341a647078c8215b4f88af0963c
Aisha <53790256+Aisha284@users.noreply.github.com>
2022-08-11 23:22:39 +0000

    adds new links

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-//rerouting 
-var http = require("http"); 
-http.createServer(function(req,res) {
-  var path = req.url.toLowerCase(); //makes lowercase
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page'); //return homepage
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page'); //returns abt page
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');  //returns nor found
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.get('/', (req, res) => {
+  res.send('Ola From Express!')
+})
 
-
-
-// var http = require("http");
-// http.createServer(function(req,res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Aloha world');
-// }).listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
